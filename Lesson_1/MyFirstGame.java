4de503195d2744181d43a330c4d199a040eafeb0
Сергей Иващенко <serege@hotmail.com>
2022-08-17 11:45:05 +0300

    HW_1 | MyFirstGame | update #2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -5,18 +5,18 @@ public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("\n2. Игра \"Угадай число\"\n");
 
-        int proposedNum = 50;
+        int targetNum = 50;
         int guessedNum;
         Random generator = new Random();
 
         do {
             guessedNum = generator.nextInt(100) + 1;
-            if (guessedNum < proposedNum) {
+            if(guessedNum < targetNum) {
                 System.out.println("число " + guessedNum + " меньше того, что загадал компьютер");
-            } else if (guessedNum > proposedNum) {
+            } else if(guessedNum > targetNum) {
                 System.out.println("число " + guessedNum + " больше того, что загадал компьютер");
             }
-        } while(proposedNum != guessedNum);
+        } while(targetNum != guessedNum);
         System.out.println(guessedNum + "! Вы победили!");
     }
 }
