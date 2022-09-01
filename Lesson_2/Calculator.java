57b2a1d1a5c9f3f350c6ec4b1a77a7e93d7c5324
Сергей Иващенко <serege@hotmail.com>
2022-09-01 21:19:31 +0300

    HW_2 | update #6

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -4,46 +4,38 @@ public class Calculator {
     private int num2;
     private char sign;
 
-    public boolean setParams(int num1, char sign, int num2) {
-        if(!(sign == '+' || sign == '-' || sign == '*' || sign == '/' || sign == '^' || sign == '%')) {
-            System.out.println("Неверный знак математической операции");
-            return false;
-        }
-        if((num2 == 0) && (sign == '/' || sign == '%')) {
-            System.out.println("Деление на ноль");
-            return false;
-        } else {
-            this.num1 = num1;
-            this.sign = sign;
-            this.num2 = num2;
-            return true;
-        }
+    public Calculator(int num1, char sign, int num2) {
+        this.num1 = num1;
+        this.sign = sign;
+        this.num2 = num2;
     }
 
-    public double getResult() {
+    public String calculateExpr() {
+        if((num2 == 0) && (sign == '/' || sign == '%')) {
+            return "деление на ноль";
+        }
         switch(sign) {
             case '+':
-                return num1 + num2;
+                return "" + (num1 + num2);
             case '-':
-                return num1 - num2;
+                return "" + (num1 - num2);
             case '*':
-                return num1 * num2;
+                return "" + (num1 * num2);
             case '/':
-                return (double) num1 / (double) num2;
+                return "" + (double) num1 / (double) num2;
             case '^':
                 if(num1 == 0) {
-                    return 0;
-                } else {
-                    int result = 1;
-                    for (int i = 1; i <= num2; i++) {
-                        result *= num1;
-                    }
-                    return result;
+                    return "0";
                 }
+                int result = 1;
+                for (int i = 1; i <= num2; i++) {
+                    result *= num1;
+                }
+                return "" + result;
             case '%':
-                return num1 % num2;
+                return "" + num1 % num2;
             default:
-                return 0;
+                return "неверный знак математической операции";
         }
     }
 }
