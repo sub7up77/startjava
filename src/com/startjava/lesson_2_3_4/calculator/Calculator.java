9e678daaf35d467bc6811af98563713cfe784586
Сергей Иващенко <serege@hotmail.com>
2022-10-12 19:04:39 +0300

    HW_4 | update #7

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -4,13 +4,51 @@ import static java.lang.Math.*;
 import static java.lang.Integer.parseInt;
 
 public class Calculator {
+    private static int num1;
+    private static char sign;
+    private static int num2;
 
-    public static double calculate(String[] str) {
+    public static boolean isCorrectExpression(String expression) {
+        String[] str = expression.split(" ");
 
-        int num1 = parseInt(str[0]);
-        char sign = str[1].charAt(0);
-        int num2 = parseInt(str[2]);
+        if (str.length != 3 || str[1].equals("") || !setSign(str[1]) || !setNums(str[0], str[2])) {
+            System.out.println("Недопустимое математическое выражение");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    private static boolean setNums(String str1, String str2) {
+        try {
+            num1 = parseInt(str1);
+            num2 = parseInt(str2);
+            if(num1 <= 0 || num2 <= 0) {
+                System.out.println("Операнды должны быть положительными числами");
+                return false;
+            } else {
+                return true;
+            }
+        } catch(NumberFormatException ex) {
+            System.out.println("Операнд не является числом");
+            return false;
+        }
+    }
+
+    private static boolean setSign(String str) {
+        char[] signs = {'+', '-', '*', '/', '^', '%'};
+
+        for (char ch : signs) {
+            if(ch == str.charAt(0)) {
+                sign = ch;
+                return true;
+            }
+        }
+        System.out.println("Недопустимый знак математической операции");
+        return false;
+    }
+
+    public static double calculate() {
         return switch(sign) {
             case '+' -> num1 + num2;
             case '-' -> num1 - num2;
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,62 +1,23 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
-import static java.lang.Integer.parseInt;
 
 public class CalculatorTest {
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-        String input;
+        String expression;
 
         do {
-            String[] str;
             do {
                 System.out.print("\nВведите математическое выражение, разделители - пробелы: ");
-                input = in.nextLine();
-                str = input.split(" ");
-            } while(!isPermitted(str));
-            System.out.println(input + " = " + Calculator.calculate(str) + "\n");
+                expression = in.nextLine();
+            } while(!Calculator.isCorrectExpression(expression));
+            System.out.println(expression + " = " + Calculator.calculate() + "\n");
             do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                input = in.nextLine();
-            } while(!input.equals("yes") && !input.equals("no"));
-        } while(input.equals("yes"));
-    }
-
-    private static int getNum(String str) {
-        try {
-            int num = parseInt(str);
-            if(num <= 0) {
-                System.out.println("Операнды должны быть положительными числами");
-                return 0;
-            } else {
-                return num;
-            }
-        } catch(NumberFormatException ex) {
-            System.out.println("Операнд не является числом");
-            return 0;
-        }
-    }
-
-    private static boolean isSign(String str) {
-        char[] sign = {'+', '-', '*', '/', '^', '%'};
-        for (char ch : sign) {
-            if(str.charAt(0) == ch) {
-                return true;
-            }
-        }
-        System.out.println("Недопустимый знак математической операции");
-        return false;
-    }
-
-    private static boolean isPermitted(String[] str) {
-        if (str.length != 3 || str[1].equals("") || getNum(str[0]) == 0 || !isSign(str[1]) || getNum(str[2]) == 0) {
-            System.out.println("Недопустимое математическое выражение");
-            return false;
-        } else {
-            return true;
-        }
+                expression = in.nextLine();
+            } while(!expression.equals("yes") && !expression.equals("no"));
+        } while(expression.equals("yes"));
     }
 }
